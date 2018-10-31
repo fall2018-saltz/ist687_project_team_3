c89256310b27e8d82916c212205658f18ffb1006
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 15:53:21 -0400

    Saving Exploration1.R

--- Exploration1.R
+++ Exploration1.R
@@ -6,7 +6,7 @@ df1<- df %>%
         group_by(Airline.Status) %>%
         summarise(n=n(),mean= mean(as.numeric(Satisfaction)),sd= sd(as.numeric(Satisfaction))
                   ,max= max(as.numeric(Satisfaction)),min= min(as.numeric(Satisfaction)))
-df1
+#df1
 #plotting Satisfaction based on airlineStatus
 #plot1 <- ggplot(df,aes(Airline.Status,fill=Satisfaction))+
 #                geom_bar()
