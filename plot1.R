c9cb0027185e931a605649c1575cc0794a64d83f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 13:11:39 -0400

    Saving plot1.R

--- plot1.R
+++ plot1.R
@@ -4,7 +4,8 @@ library(dplyr)
 plot1 <- ggplot(df,aes(Airline.Status,fill=Satisfaction))+
                 geom_bar()
 
-    df %>% 
-        filter(Satisfaction=='1') %>%
+df1<-    df %>% 
+        filter(Satisfaction=='1') 
+        
                 
 plot2 <-
