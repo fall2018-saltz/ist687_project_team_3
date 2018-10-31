239635d75606040e6f99d5ddee5f3c828280a7e4
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 15:53:41 -0400

    Saving Exploration1.R

--- Exploration1.R
+++ Exploration1.R
@@ -1,11 +1,11 @@
 
 library(dplyr)
 #summarizing dataframe
-df1<- df %>% 
-        filter(Flight.cancelled=='0') %>%
-        group_by(Airline.Status) %>%
-        summarise(n=n(),mean= mean(as.numeric(Satisfaction)),sd= sd(as.numeric(Satisfaction))
-                  ,max= max(as.numeric(Satisfaction)),min= min(as.numeric(Satisfaction)))
+#df1<- df %>% 
+      #  filter(Flight.cancelled=='0') %>%
+       # group_by(Airline.Status) %>%
+       # summarise(n=n(),mean= mean(as.numeric(Satisfaction)),sd= sd(as.numeric(Satisfaction))
+        #          ,max= max(as.numeric(Satisfaction)),min= min(as.numeric(Satisfaction)))
 #df1
 #plotting Satisfaction based on airlineStatus
 #plot1 <- ggplot(df,aes(Airline.Status,fill=Satisfaction))+
