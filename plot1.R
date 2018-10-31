6a50d74acc7b91381647cb2e79c2b4f41fea02c7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-10-31 13:09:39 -0400

    Saving plot1.R

--- plot1.R
+++ plot1.R
@@ -1,5 +1,7 @@
 
+library(dplyr)
 #plotting Satisfaction based on airlineStatus
 plot1 <- ggplot(df,aes(Airline.Status,fill=Satisfaction))+
                 geom_bar()
-plot2 <- 
+                
+plot2 <-
