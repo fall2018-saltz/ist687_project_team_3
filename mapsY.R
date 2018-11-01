d50c0f8c66014fd0d6095d2f06fe9ed1609a857f
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 12:06:11 -0400

    Saving mapsY.R

--- mapsY.R
+++ mapsY.R
@@ -5,6 +5,10 @@ library(ggplot2)
 
 df <- raw_data
 str(df)
+
+df["orgin.city_low"] <- tolower(df$Orgin.City)
+df["dest.city_low"] <- tolower(df$Destination.City)
+
 source_fl <- unique(df$Orgin.City)
 dest_fl <- unique(df$Destination.City)
 source_co <- geocode(source="dsk", source_fl)
