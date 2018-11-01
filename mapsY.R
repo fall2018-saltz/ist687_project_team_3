6323fb0d1489797b6bbaa4e9236fcb608805719b
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 11:39:19 -0400

    Saving mapsY.R

--- mapsY.R
+++ mapsY.R
@@ -1,8 +1,9 @@
 
 library(ggplot2)
+library(maps)
 library(ggmap)
 df <- raw_data
 
 source_fl <- unique(df$Orgin.City)
 dest_fl <- unique(df$Destination.City)
-
+source_co <- geocode(source= "dsk", source_fl)
