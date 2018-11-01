ee99e90e3f6c13e48db945da398ebde7dfec7ea7
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-01 09:58:00 -0400

    Saving Exploration1.R

--- Exploration1.R
+++ Exploration1.R
@@ -28,7 +28,7 @@ df3 <- df[df$Age<21,]
 mean1<- mean(df3$Age)
 df4 <- df[between(df$Age,21,40),]
 mean4<- mean(df4$Age)
-df5 <- df[df$Age>=40,
+df5 <- df[df$Age>=40,]
 mean5<- mean(df5$Age)]
 #the middle age group is likely to give higher rating compared to others
 
