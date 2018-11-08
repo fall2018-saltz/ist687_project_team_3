6a080979f41f11294b146cfed151e895433dc802
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-08 11:52:25 -0500

    deleting cleaning_1.R

--- cleaning_1.R
+++
@@ -1,7 +0,0 @@
-
-# convert date info in format 'mm/dd/yyyy', template code to change dates
-strDates <- c("01/05/1965", "08/16/1975")
-dates <- as.Date(strDates, "%m/%d/%Y")
-#comments here
-##feature engineering 
-##seperate month, day, year into columns 
