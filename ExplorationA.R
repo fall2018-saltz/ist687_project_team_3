042be20df2d36eaf428dd847565b02f42404b161
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-08 12:11:44 -0500

    Saving ExplorationA.R

--- ExplorationA.R
+++ ExplorationA.R
@@ -10,6 +10,5 @@ df1<- df %>%
         summarise(n=n(),mean= mean(as.numeric(Satisfaction)),sd= sd(as.numeric(Satisfaction))
                   ,max= max(as.numeric(Satisfaction)),min= min(as.numeric(Satisfaction)))
 #plotting Satisfaction based on airlineStatus
-plot1 <- ggplot(df,aes(Airline.Status,fill=Satisfaction))+
-                    geom_bar(position='fill')
+plot1 <- ggplot(df,aes(Airline.Status,fill=Satisfaction))+ geom_bar(position='fill')
 #NOTE - in R studio the graph is coloured properly based on Satisfaction
