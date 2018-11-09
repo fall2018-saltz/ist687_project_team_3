8d3fb2a2236e0e9b544a213579a8ba4033486ab2
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-08 20:35:55 -0500

    Saving cleaning_2.R

--- cleaning_2.R
+++ cleaning_2.R
@@ -1,22 +1,22 @@
 
 df<-raw_data
 # Encoding categorical variables
-df$Gender = as.factor(df$Gender,
+df$Gender = factor(df$Gender,
                          levels = c('Female','Male'),
                          labels = c(0, 1))
-df$Airline.Status =  as.factor(df$Airline.Status,
+df$Airline.Status =  factor(df$Airline.Status,
                            levels = c('Blue', 'Silver','Gold','Platinum'),
                            labels = c(0, 1, 2, 3))
-df$Type.of.Travel =  as.factor(df$Type.of.Travel,
+df$Type.of.Travel =  factor(df$Type.of.Travel,
                            levels = c('Business travel', 'Personal Travel','Mileage tickets'),
                            labels = c(0, 1, 2))
 df$Class =  factor(df$Class,
                   levels = c('Business','Eco', 'Eco Plus'),
                    labels = c(0, 1,2))
-df$Flight.cancelled = as.factor(df$Flight.cancelled,
+df$Flight.cancelled = factor(df$Flight.cancelled,
                              levels = c('No','Yes'),
                              labels = c(0, 1))
-df$Arrival.Delay.greater.5.Mins = as.factor(df$Arrival.Delay.greater.5.Mins,
+df$Arrival.Delay.greater.5.Mins = factor(df$Arrival.Delay.greater.5.Mins,
                              levels = c('no','yes'),
                              labels = c(0, 1))
 
