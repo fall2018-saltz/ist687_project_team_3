5224ec00b52282e6cbf2d608f007f02b5ae8762d
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-14 15:47:54 -0500

    Saving mathematical_data_exploration.R

--- mathematical_data_exploration.R
+++ mathematical_data_exploration.R
@@ -27,6 +27,7 @@ legend(x = "right", legend.text
        ,bty="n", inset=c(-0.08,0), xpd = TRUE)
 
 # 2. Shopping.Amount.at.Airport, colored scatterplot
+library(plotly)
 scatterplot1 <- plot_ly(df, x = ~Shopping.Amount.at.Airport, y = ~Satisfaction, type="scatter", text = paste("Shopping.Amount: ", df$Shopping.Amount.at.Airport),
           mode = "markers", color = ~Shopping.Amount.at.Airport, size = ~Shopping.Amount.at.Airport)
 scatterplot1
