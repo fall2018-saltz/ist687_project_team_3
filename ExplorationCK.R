6577c9934be6bb2fbe251d462c4ac4f2db9bd627
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-14 21:42:29 -0500

    Saving ExplorationCK.R

--- ExplorationCK.R
+++ ExplorationCK.R
@@ -1,6 +1,6 @@
 
 library(dplyr)
-myData <- read.csv("clean_data.csv")
+myData <- clean_data
 
 #Histogram for eating and drinking variable
 hist(myData$Eating.and.Drinking.at.Airport, breaks = 20, xlab = "Eating/Drinking at Airport", main = "Histogram Eating and Drinking At Airport")
