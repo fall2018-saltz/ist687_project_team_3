e6b60f3fe77b220b02ac9c97614926daffd50117
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-11-14 21:55:16 -0500

    Saving ExplorationCK.R

--- ExplorationCK.R
+++ ExplorationCK.R
@@ -3,11 +3,11 @@ library(dplyr)
 myData <- read.csv("clean_data.csv")
 
 #Histogram for eating and drinking variable
-hist(myData$Eating.and.Drinking.at.Airport, breaks = 20, xlab = "Eating/Drinking at Airport", main = "Histogram Eating and Drinking At Airport")
+histEatingAndDrinking <- hist(myData$Eating.and.Drinking.at.Airport, breaks = 20, xlab = "Eating/Drinking at Airport", main = "Histogram Eating and Drinking At Airport")
 #Histogram for day of month
 hist(myData$Day.of.Month, breaks = 20,xlab = "Day of Month", main= "Histogram Day of the Month")
 #Histogram for scheduled depature hour variable
-hist(myData$Scheduled.Departure.Hour, breaks = 10, xlab = "Departure Hour", main= "Histogram Depature Hour")
+histScheduledDepartureHour <- hist(myData$Scheduled.Departure.Hour, breaks = 10, xlab = "Departure Hour", main= "Histogram Depature Hour")
 #Histogram for depature delay in minutes
 hist(myData$Departure.Delay.in.Minutes, breaks = 15,xlab = "Delay in Minutes", main= "Histogram Depature Delay")
 
