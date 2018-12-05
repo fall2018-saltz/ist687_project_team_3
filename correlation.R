a68985bb4bc32f8c6506eb0eeec2a39d143ff9af
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-04 22:31:02 -0500

    Saving correlation.R

--- correlation.R
+++ correlation.R
@@ -4,6 +4,6 @@
 df$Satisfaction <- as.integer(df$Satisfaction)
 
 #library(corrplot)
-#df<-na.omit(df)
+df<-na.omit(df)
 #M <- cor(df[c(1,3,5,6,7,8,10,11,12,14,22,23,24,26,27)])
 #correlationplot<-corrplot(M, method = "color",type='upper')
