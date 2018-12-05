3fc6faa2bd3c3a8bb20044b29f55ddadc31537ba
yhegdesu <36450788+yhegdesu@users.noreply.github.com>
2018-12-04 22:17:15 -0500

    deleting correlations.R

--- correlations.R
+++
@@ -1,9 +0,0 @@
-
-setwd("D:/ADS/IST 687/project")
-df <-read.csv("Satisfaction Survey.csv")
-df$Satisfaction <- as.integer(as.character(df$Satisfaction))
-
-library(corrplot)
-df<-na.omit(df)
-M <- cor(df[c(1,3,5,6,7,8,10,11,12,14,22,23,24,26,27)])
-corrplot.mixed(M, method = "color",type='upper')
